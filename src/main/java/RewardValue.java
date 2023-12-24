02172129d2b49164eae55f1c18689d4474453c55
ivan <ivanbanks@icloud.com>
2023-12-23 22:09:00 -0600

    added reward value converter

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,24 @@
 public class RewardValue {
+    private double cashValue;
+    private int milesValue;
+
+    // Conversion rate constant
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    };
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+        this.cashValue = milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    }
+
+    public int getMilesValue(){
+        return milesValue;
+    };
+
+    public double getCashValue() {
+        return cashValue;
+    }
 }
